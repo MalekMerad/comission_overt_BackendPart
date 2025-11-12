a87567f9dbedf7689bcb23bae6aa64574d37dfa8
Malek Merad <anamalekmerad2003@gmail.com>
2025-11-12 16:41:12 +0100

    Delete Backend directory

--- Backend/Routes/lotRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const lotController = require('../Controllers/lotsController/lotController');
-
-router.post('/addLot', lotController.insertLotSqlServer); 
-router.get('/getLots', lotController.getAllLotsSqlServer);
-
-module.exports = router;

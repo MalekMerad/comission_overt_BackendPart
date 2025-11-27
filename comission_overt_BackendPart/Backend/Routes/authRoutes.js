5f2c0f534673617486a263672a3ffd0f475bdc77
Malek Merad <anamalekmerad2003@gmail.com>
2025-11-27 12:50:31 +0100

    Delete comission_overt_BackendPart/Backend directory

--- comission_overt_BackendPart/Backend/Routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../Controllers/User controller/authController');
-
-router.post("/login", authController.loginSqlServer);
-module.exports = router;
-
-
--- comission_overt_BackendPart/Backend/Routes/operationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const operationsController = require('../Controllers/operationsController/operationController');
-
-router.post('/addOperation', operationsController.insertOperationSqlServer);
-router.get('/AllOperations', operationsController.getAllOperationsSqlServer);
-router.delete('/deleteOperation/:NumOperation', operationsController.deleteOperationSqlServer);
-
-module.exports = router;

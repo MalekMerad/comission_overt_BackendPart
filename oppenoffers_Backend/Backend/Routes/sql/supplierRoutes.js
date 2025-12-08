4b8cdb9c3390f9516fff8bf4918176c6cf8e0538
Malek Merad <anamalekmerad2003@gmail.com>
2025-12-08 14:12:55 +0100

    Delete oppenoffers_Backend/Backend directory

--- oppenoffers_Backend/Backend/Routes/sql/supplierRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const supplierController = require('../../Controllers/sql/supplierController');
-
-router.post('/addSupplier', supplierController.addSupplier);
-
-module.exports = router;
